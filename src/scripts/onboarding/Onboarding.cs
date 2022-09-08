63b8ee28fb0d20a2ff49300012364be077244158
Mike <mishakozlov74@gmail.com>
2022-09-08 10:46:13 +0200

    Some bindings for welcome screen

--- src/scripts/onboarding/Onboarding.cs
+++ src/scripts/onboarding/Onboarding.cs
@@ -1,9 +1,12 @@
 using Godot;
 
 namespace TinyScreen.scripts.onboarding {
-    public class Onboarding : Node {
+    public class Onboarding : Control {
 
-        public enum Stage {
+        [Export] public PackedScene WelcomeScene;
+        [Export] public PackedScene UpdateScene;
+
+        private enum Stage {
             Welcome,
             Update,
             Library,
@@ -11,9 +14,49 @@ namespace TinyScreen.scripts.onboarding {
             Device,
             Widgets
         }
+
+        private Stage _currentStage;
+        private Node _currentScene;
         
         public override void _Ready() {
             GD.Print("Onboarding is started!");
+            
+            SetStage(_currentStage);
         }
+        
+        private void SetStage(Stage newStage) {
+            
+            switch (newStage) {
+                case Stage.Welcome:
+                    TransitionToScene(WelcomeScene);
+                    break;
+                
+                case Stage.Update:
+                    GD.Print("Update!!!");
+                    break;
+            }
+        }
+
+        public void NextStage() {
+            if (_currentStage != Stage.Widgets) {
+                SetStage(_currentStage + 1);
+                return;
+            }
+            
+            // At this point onboarding is finished
+            // Persist settings and games
+        }
+
+        private async void TransitionToScene(PackedScene newScene) {
+            
+            // If there is no scene, just show it (for now)
+            if (_currentScene == null) {
+                _currentScene = newScene.Instance();
+                AddChild(_currentScene);
+            }
+            
+            // Otherwise replace it with animation
+        }
+        
     }
 }
---
+++ src/scripts/onboarding/Welcome.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+
+namespace TinyScreen.scripts.onboarding {
+    public class Welcome : Node {
+
+        [Export] public NodePath StartButtonPath;
+        
+        private Button _startButton;
+        
+        public override void _Ready() {
+            base._Ready();
+            _startButton = GetNode<Button>(StartButtonPath);
+            _startButton.Connect("pressed", GetParent(), nameof(Onboarding.NextStage));
+        }
+    }
+}
