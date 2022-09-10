85520d037fb3294b6f8066ac14c67576d0c9f4fc
Mike <mishakozlov74@gmail.com>
2022-09-10 15:29:39 +0200

    Upgrade project structure to new style.

--- plugins/SteamLibrarySource/SteamLibrarySource.cs
+++
@@ -1,9 +0,0 @@
-﻿using TinyScreen.Framework.Interfaces;
-
-namespace SteamLibrarySource {
-    public class SteamLibrarySource: ILibrarySource {
-        public string GetName() {
-            return "Steam";
-        }
-    }
-}
