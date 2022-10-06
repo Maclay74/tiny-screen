be6b50fd534890148e770c80d6bb2146e2d12007
Mike <mishakozlov74@gmail.com>
2022-10-07 01:34:56 +0200

    Clean up, rebase

--- Plugins/SteamGridGameDataProvider/SteamGridGameDataProvider.cs
+++ Plugins/SteamGridGameDataProvider/SteamGridGameDataProvider.cs
@@ -5,25 +5,32 @@ using Common.Interfaces;
 using craftersmine.SteamGridDBNet;
 
 namespace SteamGridGameDataProvider {
-    
-    public class SteamGridGameDataProvider : IGameDataProvider, IGameDataProvider<ArtworkGameDataType, string?>, IGameDataProvider<BackgroundGameDataType, string?> 
-    {
+    public class SteamGridGameDataProvider : IGameDataProvider, IGameDataProvider<ArtworkGameDataType, string?>,
+        IGameDataProvider<BackgroundGameDataType, string?> {
         public int Priority() => 10;
-        
+
         private SteamGridDb _client;
-        
-        public SteamGridGameDataProvider() 
-        {
+
+        public SteamGridGameDataProvider() {
             _client = new SteamGridDb("c6c3d47a9c8deeff0f9481aea822bec1");
         }
+        
+        async Task<string?> IGameDataProvider<ArtworkGameDataType, string?>.GetData(ArtworkGameDataType dataType,
+            string gameName) {
+            return await GetImageUrlByGameName(gameName, SteamGridDbDimensions.W600H900);
+        }
 
-        async Task<string?> GetData(string gameName, SteamGridDbDimensions steamGridDbDimensions) 
-        {
+        async Task<string?> IGameDataProvider<BackgroundGameDataType, string?>.GetData(BackgroundGameDataType dataType,
+            string gameName) {
+            return await GetImageUrlByGameName(gameName, SteamGridDbDimensions.W920H430);
+        }
+
+        private async Task<string?> GetImageUrlByGameName(string gameName, SteamGridDbDimensions steamGridDbDimensions) {
             try {
                 var games = await _client.SearchForGamesAsync(gameName);
-                
-                if (games.Length == 0) return null;
-                
+
+                if (games == null || games.Length == 0) return null;
+
                 var grids = await _client.GetGridsByGameIdAsync(
                     games[0].Id,
                     false,
@@ -34,23 +41,11 @@ namespace SteamGridGameDataProvider {
                     SteamGridDbTypes.Static
                 );
 
-                if (grids.Length == 0) return null;
-
-                return grids[0].FullImageUrl;
+                return grids?[0].FullImageUrl;
             }
             catch (Exception) {
                 return null;
             }
-        }
-        
-        async Task<string?> IGameDataProvider<ArtworkGameDataType, string?>.GetData(ArtworkGameDataType dataType, string gameName)
-        {
-            return await GetData(gameName, SteamGridDbDimensions.W600H900);
-        }
-
-        async Task<string?> IGameDataProvider<BackgroundGameDataType, string?>.GetData(BackgroundGameDataType dataType, string gameName)
-        {
-            return await GetData(gameName, SteamGridDbDimensions.W660H930);
         }
     }
 }
